5ddb02181c10782f8e92d79f03703c2717d6fb70
DiegoCV <skdiegomxtr@gmail.com>
2019-11-21 11:48:35 -0500

    'NuevoAvance'

---
+++ lineas/__pycache__/Linea.py
@@ -0,0 +1,18 @@
+class Linea:
+	def __init__(self, x_0, y_0, x_1, y_1):
+		self.x_0 = x_0
+		self.y_0 = y_0
+		self.x_1 = x_1
+		self.y_1 = y_1
+
+	def get_x_0():
+		return self.x_0
+
+	def get_y_0():
+		return self.y_0
+
+	def get_x_1():
+		return self.x_1
+
+	def get_y_1():
+		return self.y_1
--- lineas/lineas.py
+++ lineas/lineas.py
@@ -1,6 +1,7 @@
 
 import pygame 
-import time
+import numpy as np
+from Linea import *
 ANCHO = 700
 LARGO = 500
 pygame.init()
@@ -19,10 +20,11 @@ hecho = False
 x = ANCHO/2
 y = LARGO*0.8
 x_t = 100
-y_t = 50
+y_t = 100
 
-ancho_juego = 5
-largo_juego = 5
+columnas = 3
+filas = 3
+nodos = filas * columnas
 
 # Se usa para establecer cuan rápido se actualiza la pantalla
 var_izq = False
@@ -30,9 +32,24 @@ var_der = False
 var_arr = False
 var_aba = False
 
+#ANCHO ENTRE LOS PUNTOS 
+sep = 20
+
+#VARIABLES PARA CONTROLAR LA POSICION DE LA LINEA D E POSICION ACTUAL
+lc_coord_ini = [x_t, y_t]
+lc_coord_fin = [x_t + sep, y_t]
+
+#Lineas definitivas
+lineas_final = []
+
+#Matriz con el estado actual del juego filas columnas
+matriz_estado = np.zeros((nodos, nodos), dtype=bool)
+print(matriz_estado)
+print(matriz_estado.item((1,1)))
+print(lc_coord_fin)
 reloj = pygame.time.Clock()
 
-def pintar_puntos(can_x, can_y, sep, coord_ini):
+def pintar_puntos(can_x, can_y, sep, coord_ini):   
     i = 0
     j = 0
     v_x = coord_ini[0]
@@ -46,6 +63,87 @@ def pintar_puntos(can_x, can_y, sep, coord_ini):
         v_x = coord_ini[0]
         v_y = v_y + sep
         j = j + 1
+    
+
+def pintar_lineas_final():
+    for linea in lineas_final:
+        pintar_linea((linea.get_x_0(), linea.get_y_0()), (linea.get_x_1(), linea.get_y_1()), AZUL)
+        #pintar_linea([linea["c_ini"][0], linea["c_ini"][1]], [linea["c_fin"][0], linea["c_fin"][1]], AZUL)
+        #print()
+
+#Se encarga de comprobar si la ultima linea puesta encierra un cuadrado
+def validar_cuadro(c_ini, c_fin):
+    if esta_vertical_linea(c_ini, c_fin):
+        #Valido la linea del frente
+        '''
+            ->, |v, <- ,|A 
+        '''
+        c_ini_frente = c_ini
+        c_ini_frente[1] = c_ini_frente[1] + sep
+        nodo_1 = obtener_nodo(c_ini)
+        nodo_2 = obtener_nodo(c_ini_frente)
+        return hay_relacion(nodo_1, nodo_2)
+
+    elif esta_horizontal_linea(c_ini, c_fin):
+        return 1
+        
+def hay_relacion(nodo_1, nodo_2):
+    return matriz_estado.item(nodo_1, nodo_2)   
+
+def obtener_nodo(c_ini):
+    return columnas*parsear_coordenada(c_ini)[0]+parsear_coordenada(c_ini)[1]
+
+def pintar_linea(c_ini, c_fin, color):
+    pygame.draw.line(pantalla, color, c_ini, c_fin, 1)
+
+def mover_lc_arriba():
+    lc_coord_ini[1] = lc_coord_ini[1] - sep
+    lc_coord_fin[1] = lc_coord_fin[1] - sep
+
+def mover_lc_abajo():
+    lc_coord_ini[1] = lc_coord_ini[1] + sep
+    lc_coord_fin[1] = lc_coord_fin[1] + sep
+
+def mover_lc_izquierda():
+    lc_coord_ini[0] = lc_coord_ini[0] - sep
+    lc_coord_fin[0] = lc_coord_fin[0] - sep
+
+def mover_lc_derecha():
+    lc_coord_ini[0] = lc_coord_ini[0] + sep
+    lc_coord_fin[0] = lc_coord_fin[0] + sep
+
+def cambiar_posicion():
+    if esta_vertical_linea_principal():
+        lc_coord_fin[0] = lc_coord_ini[0]
+        lc_coord_fin[1] = lc_coord_ini[1] + sep
+    elif esta_horizontal_linea_principal():
+        lc_coord_fin[0] = lc_coord_ini[0] + sep
+        lc_coord_fin[1] = lc_coord_ini[1]
+
+def esta_vertical_linea_principal():
+    return lc_coord_ini[1] == lc_coord_fin[1]
+
+def esta_horizontal_linea_principal():
+    return lc_coord_ini[0] == lc_coord_fin[0]
+
+def esta_vertical_linea(c_ini, c_fin):
+    return c_ini[1] == c_fin[1]
+
+def esta_horizontal_linea(c_ini, c_fin):
+    return c_ini[0] == c_fin[0]
+
+def registrar_linea(c_ini, c_fin):
+    #lineas_final.append({"c_ini":c_ini, "c_fin":c_fin})
+    lineas_final.append(Linea(c_ini[0],c_ini[1],c_fin[0],c_fin[1]))
+    nodo_1 = columnas*parsear_coordenada(c_ini)[0]+parsear_coordenada(c_ini)[1]
+    nodo_2 = columnas*parsear_coordenada(c_fin)[0]+parsear_coordenada(c_fin)[1]
+    matriz_estado.itemset((nodo_1,nodo_2), True)
+    matriz_estado.itemset((nodo_2,nodo_1), True)
+    #print(matriz_estado)
+ 
+def parsear_coordenada(coordenada):
+    return (int((coordenada[1] - 100)/20), int((coordenada[0] - 100)/sep))
+
 
 # -------- Bucle principal del Programa -----------
 while not hecho:
@@ -56,24 +154,18 @@ while not hecho:
             hecho = True
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_w:
-                if y - vel > y_1:
-                    y -= vel
-                    var_arr = True 
+                mover_lc_arriba()
             elif event.key == pygame.K_a:
-                if x - vel > x_1:
-                    x -= vel
-                    var_izq = True
+                mover_lc_izquierda()
             elif event.key == pygame.K_s:
-                if y - vel < y_2:
-                    y += vel
-                    var_aba = True
+                mover_lc_abajo()
             elif event.key == pygame.K_d:
-                if x + vel < x_2:
-                    x += vel
-                    var_der = True
+                mover_lc_derecha()
+            elif event.key == pygame.K_f:
+                cambiar_posicion()
             elif event.key == pygame.K_r:
-                if(perdio == True):                          
-                    perdio = False                                   
+                registrar_linea(lc_coord_ini, lc_coord_fin)
+                print(validar_cuadro(lc_coord_ini, lc_coord_fin))
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_w:
                 var_arr = False 
@@ -102,8 +194,15 @@ while not hecho:
  
     pantalla.fill(BLANCO)
     actual = pygame.draw.rect(pantalla, ROJO, [x, y, 20, 20], 0)
+    pintar_lineas_final()
+    pintar_puntos(columnas, filas, sep, [100,100])
+    #pintar_linea(lc_coord_ini, lc_coord_fin, VERDE)
+    pintar_linea(lc_coord_ini, lc_coord_fin, VERDE)
+    #pintar_linea([120,120], [140,140], VERDE)
+    #pintar_linea([160,160], [180,180], VERDE)
+    #pintar_lineas_final()
     
-    pintar_puntos(10, 5, 20, [100,100])
+    
     pygame.display.flip()
     reloj.tick(60)
      
